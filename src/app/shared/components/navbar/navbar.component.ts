4e212955d3312f02ac8bd72a55a43e78d29ba2bf
MarioAG <marioalvarezgonzalez21@gmail.com>
2026-01-23 16:25:03 +0100

    221. Titulo en el NavBar - Observable - Señal

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,8 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { routes } from '../../../app.routes';
+import { NavigationEnd, Router, RouterLink } from '@angular/router';
+import { filter, map, tap } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [AsyncPipe, RouterLink],
   templateUrl: './navbar.component.html',
 })
-export class NavbarComponent { }
+export class NavbarComponent {
+  router = inject(Router);
+
+  routes = routes
+    .map((route) => ({
+      path: route.path,
+      title: `${route.title ?? 'Mapas en Angular'}`,
+    }))
+    .filter((route) => route.path !== '**');
+
+  pageTitle$ = this.router.events.pipe(
+    filter((event) => event instanceof NavigationEnd),
+    // tap((event) => console.log(event)),
+    map((event) => event.url),
+    map((url) => routes.find((route) => `/${route.path}` === url)?.title ?? 'Mapas'),
+  );
+
+  // Con señales y habra que poner en el html en vez de pageTitle$|async solo pageTitle()
+  pageTitle = toSignal(
+    this.router.events.pipe(
+      filter((event) => event instanceof NavigationEnd),
+      // tap((event) => console.log(event)),
+      map((event) => event.url),
+      map((url) => routes.find((route) => `/${route.path}` === url)?.title ?? 'Mapas'),
+    ),
+  );
+}
