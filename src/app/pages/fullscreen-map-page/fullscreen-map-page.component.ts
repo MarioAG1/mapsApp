ac18ba81e47d438118a44a589c70ef7acfc8cd3a
MarioAG <marioalvarezgonzalez21@gmail.com>
2026-01-26 14:03:50 +0100

    224. Mapa a pantalla completa

--- src/app/pages/fullscreen-map-page/fullscreen-map-page.component.ts
+++ src/app/pages/fullscreen-map-page/fullscreen-map-page.component.ts
@@ -1,8 +1,36 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, viewChild } from '@angular/core';
+import mapboxgl from 'mapbox-gl';
+import { environment } from '../../../environments/environment.development';
+
+mapboxgl.accessToken = environment.mapboxKey;
 
 @Component({
   selector: 'app-fullscreen-map-page',
   imports: [],
   templateUrl: './fullscreen-map-page.component.html',
+  styles: `
+    div {
+      width: 100vw;
+      height: calc(100vh - 64px);
+    }
+  `,
 })
-export class FullscreenMapPageComponent { }
+export class FullscreenMapPageComponent implements AfterViewInit {
+  divElement = viewChild<ElementRef>('map');
+
+  async ngAfterViewInit() {
+    if (!this.divElement()?.nativeElement) {
+      return;
+    }
+    // Opcional, para hacer que tarde un poco para cargar bien
+    await new Promise((resolve) => setTimeout(resolve, 80));
+
+    const element = this.divElement()?.nativeElement;
+
+    const map = new mapboxgl.Map({
+      container: element, // container ID
+      center: [-74.5, 40], // starting position [lng, lat]. Note that lat must be set between -90 and 90
+      zoom: 9, // starting zoom
+    });
+  }
+}
--- src/environments/enviroment.development.ts
+++
@@ -1,5 +0,0 @@
-
-export const environment = {
-  mapboxKey:
-   "pk.eyJ1IjoibWFyaW9hZzEiLCJhIjoiY21rcjR2YjNpMHh5ZjNjc2EzNjk2ZWJsZyJ9.FEF0IGM3R0QJE-SXgoHBJQ",
-};
