ecf1c6e5f2d76473780b5d81203188edabff0244
MarioAG <marioalvarezgonzalez21@gmail.com>
2026-01-27 13:57:30 +0100

    228. Marcadores en el mapa

--- src/app/pages/markers-page/markers-page.component.ts
+++ src/app/pages/markers-page/markers-page.component.ts
@@ -1,8 +1,39 @@
-import { Component } from '@angular/core';
+import mapboxgl from 'mapbox-gl';
+import { AfterViewInit, Component, ElementRef, signal, viewChild } from '@angular/core';
+import { environment } from '../../../environments/environment.development';
 
+mapboxgl.accessToken = environment.mapboxKey;
 @Component({
   selector: 'app-markers-page',
   imports: [],
   templateUrl: './markers-page.component.html',
 })
-export class MarkersPageComponent { }
+export class MarkersPageComponent implements AfterViewInit {
+  divElement = viewChild<ElementRef>('map');
+  map = signal<mapboxgl.Map | null>(null);
+
+  async ngAfterViewInit() {
+    if (!this.divElement()?.nativeElement) {
+      return;
+    }
+    // Opcional, para hacer que tarde un poco para cargar bien
+    await new Promise((resolve) => setTimeout(resolve, 80));
+
+    const element = this.divElement()?.nativeElement;
+
+    const map = new mapboxgl.Map({
+      container: element, // container ID
+      center: [-4.027583, 39.862333], // starting position [lng, lat]. Note that lat must be set between -90 and 90
+      zoom: 14, // starting zoom
+    });
+
+    const marker = new mapboxgl.Marker({ draggable: false, color: 'black' })
+      .setLngLat([-4.027583, 39.862333])
+      .addTo(map);
+
+    marker.on('dragend', (event) => {});
+    this.mapListeners(map);
+  }
+
+  mapListeners(map: mapboxgl.Map) {}
+}
