eb13476470f39b3d417d3b4f81fca2d557db4277
MarioAG <marioalvarezgonzalez21@gmail.com>
2026-01-28 16:45:01 +0100

    231. Eliminar Marcadores

--- src/app/pages/markers-page/markers-page.component.ts
+++ src/app/pages/markers-page/markers-page.component.ts
@@ -82,4 +82,14 @@ export class MarkersPageComponent implements AfterViewInit {
       center: lngLat,
     });
   }
+
+  deleteMarker(marker: Marker) {
+    if (!this.map) {
+      return;
+    }
+    const map = this.map()!;
+
+    marker.mapboxMarker.remove();
+    this.markers.set(this.markers().filter((m) => m.id !== marker.id));
+  }
 }
